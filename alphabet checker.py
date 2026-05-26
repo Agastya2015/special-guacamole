3d3ad2a8738f42ae6d77985c303ab4be93e79ed4
Agastya2015 <agastyaverma15@gmail.com>
2026-05-26 19:48:09 +0530

    alphabet checker

---
+++ alphabet checker.py
@@ -0,0 +1,16 @@
+print("🔠WELCOME TO THE ALPHABET CHECKER!!!🔠")
+print("This program allows to check if youn have ACTUALLY enter an ALPHABET!!😤")
+print("LETS GET STARTED!!!🎉🥳✌️")
+print("...................................................................................")
+
+
+a = str(input("NOWW...Enter a CHARACTER😠: "))
+
+if a.isalpha():
+    print("Oh wow you ACTUALLY follow instructions...")
+    print("YOU HAVE ENTERED AN ALPHABET!!!🎉🎉🥳🥳🎈")
+    
+
+else:
+    print("OH MY GODNESS WHY CAN'T YOU FOLLOW INSTRUCTIONS!!!😤😤")
+    print("YOU HAVE NOT ENTERED AN ALPHABET!!!👿👿👿👿👿💢💢💢💢👺👺")
