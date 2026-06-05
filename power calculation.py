5635564fefb070101175011ffdae4fb7cc205ed0
Agastya2015 <agastyaverma15@gmail.com>
2026-06-05 19:45:12 +0530

    power calculation

---
+++ power calculation.py
@@ -0,0 +1,4 @@
+n = int(input("Enter a number whose power you want to calculate: "))
+p = int(input("Input power to be calculated: "))
+r = n ** p
+print("{0} to the power of {1} = {2}".format(n, p, r))
